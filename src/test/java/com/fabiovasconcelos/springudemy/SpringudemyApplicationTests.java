41d5240f7802ac58e5725c2863fa5b396af25b0d
Fabio Vasconcelos <fabio.avasc@gmail.com>
2021-06-16 13:45:46 -0300

    Adição dos arquivos base da API Spring

---
+++ src/test/java/com/fabiovasconcelos/springudemy/SpringudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiovasconcelos.springudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
